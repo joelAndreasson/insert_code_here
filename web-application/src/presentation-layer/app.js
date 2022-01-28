38583bc924fe40002c18b77f0b8cce35d3fcc8d1
joelAndreasson <joelaron007@gmail.com>
2022-01-28 12:49:59 +0100

    Imported skeleton code

---
+++ web-application/src/presentation-layer/app.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const variousRouter = require('./routers/various-router')
+const accountRouter = require('./routers/account-router')
+
+const app = express()
+
+// Setup express-handlebars.
+app.set('views', path.join(__dirname, 'views'))
+
+// Note: This code is for an old version of express-handlebars.
+// One should use newest version of packages.
+app.engine('hbs', expressHandlebars({
+	extname: 'hbs',
+	defaultLayout: 'main',
+	layoutsDir: path.join(__dirname, 'layouts')
+}))
+
+// Handle static files in the public folder.
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Attach all routers.
+app.use('/', variousRouter)
+app.use('/accounts', accountRouter)
+
+// Start listening for incoming HTTP requests!
+app.listen(8080, function(){
+	console.log('Running on 8080!')
+})
