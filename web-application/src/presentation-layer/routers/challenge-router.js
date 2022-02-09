a819453e3dc06a396e11c283c547eb37418b5323
joelAndreasson <joelaron007@gmail.com>
2022-02-09 09:00:25 +0100

    Moved getTodaysDate function and Challenge class to challengeManager, fixed some spelling errors

--- web-application/src/presentation-layer/routers/challenge-router.js
+++ web-application/src/presentation-layer/routers/challenge-router.js
@@ -3,32 +3,7 @@ const challengeManager = require('../../business-logic-layer/challenge-manager')
 
 const router = express.Router()
 
-function getTodaysDate(){
-	const today = new Date()
 
-	const yyyy = today.getFullYear()
-
-	let mm = today.getMonth()
-	mm = mm < 10 ? "0" + mm : mm;
-
-	let dd = today.getDay()
-	dd = dd < 10 ? "0" + dd : dd;
-
-	return yyyy + "-" + mm + "-" + dd
-}
-
-class Challenge{ // Should probably be put elsewhere
-	constructor(title, challengeText, progLanguage, difficulty, description, datePublished, numOfPlays, userId){
-		this.title = title
-		this.challengeText = challengeText
-		this.progLanguage = progLanguage
-		this.difficulty = difficulty
-		this.description = description
-		this.datePublished = datePublished
-		this.numOfPlays = numOfPlays
-		this.userId = userId
-	}
-}
 
 router.get("/create", function(request, response){
 	response.render("challenge-create.hbs")
@@ -40,11 +15,11 @@ router.post('/create', function(request, response){
 	const progLanguage = request.body.progLanguage
 	const difficulty = request.body.difficulty
 	const description = request.body.description
-	const datePublished = getTodaysDate()
+	const datePublished = challengeManager.getTodaysDate()
 	const numOfPlays = 0
-	const userId = 1 // Should get the userId of the account that crated this challenge
+	const userId = 1 // Should get the userId of the account that created this challenge
 
-	const challenge = new Challenge(title, challengeText, progLanguage, difficulty, description, datePublished, numOfPlays, userId)
+	const challenge = new challengeManager.Challenge(title, challengeText, progLanguage, difficulty, description, datePublished, numOfPlays, userId)
 
 	challengeManager.createChallenge(challenge, function(error, id){
 		// TODO: Add error handling
