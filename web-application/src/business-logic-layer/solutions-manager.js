0d8d65c98d622ed01246fc2a8a0ad456c4bfb048
joelAndreasson <joelaron007@gmail.com>
2022-02-09 12:49:38 +0100

    Changed getSolutionsByChallengeId name

--- web-application/src/business-logic-layer/solutions-manager.js
+++ web-application/src/business-logic-layer/solutions-manager.js
@@ -1,5 +1,5 @@
 const solutionsRepository = require('../data-access-layer/solutions-repository') // change name later to singular
 
-exports.getSolutionByChallengeId = function(challengeId,callback){
-    solutionsRepository.getSolutionByChallengeId(challengeId,callback)
+exports.getSolutionsByChallengeId = function(challengeId,callback){
+    solutionsRepository.getSolutionsByChallengeId(challengeId,callback)
 }
--- web-application/src/data-access-layer/solutions-repository.js
+++ web-application/src/data-access-layer/solutions-repository.js
@@ -3,15 +3,15 @@ const db = require('./db')
 /*
     Gets the solution corresponding to the specific challengeId
 */
-exports.getSolutionByChallengeId = function(challengeId, callback){
-    const query = "GET * FROM solutions WHERE challengeId = ? LIMIT 1"
+exports.getSolutionsByChallengeId = function(challengeId, callback){
+    const query = "GET * FROM solutions WHERE challengeId = ?"
     const values = [challengeId]
 
-    db.query(query,values,function(error, solution){
+    db.query(query,values,function(error, solutions){
         if(error){
             callback(['databaseError'], null) // handle errors at later date.
         }else{
-            callback([], solution)
+            callback([], solutions)
         }
     })
 }
